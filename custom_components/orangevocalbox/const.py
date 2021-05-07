77e94edb46a27d52dd60033d8f05f32e3c0b57c1
Save me <Cyr-ius@users.noreply.github.com>
2021-05-07 12:27:44 +0200

    Update const.py

--- custom_components/orangevocalbox/const.py
+++ custom_components/orangevocalbox/const.py
@@ -4,7 +4,7 @@ ATTR_TYPE = "type"
 DOMAIN = "orangevocalbox"
 JSON_HEADER = {
     "Content-Type": "application/json",
-    "User-Agent": "Mozilla/5.0 (HomeAssistant; +http://www.github.com/cyr-ius/hass-orangevocalbox) Firefox/1000",
+    "User-Agent": "Mozilla/5.0 (HomeAssistant; www.github.com/cyr-ius/hass-orangevocalbox) Firefox/1000",
 }
 ORANGE_URI = "https://login.orange.fr"
 ORANGE_VOCALBOX_URI = "https://boitevocale5w.orange.fr/backend"
